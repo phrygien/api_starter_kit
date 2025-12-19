5590f9b386dfb9cb0d8199542d82860668e598f5
Mecene <mecenephrygien@gmail.com>
2025-12-20 00:41:50 +0400

    clean:code

--- app/Http/Controllers/Auth/V1/UserController.php
+++ app/Http/Controllers/Auth/V1/UserController.php
@@ -10,10 +10,19 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController
 {
+    /**
+     * Summary of __construct
+     * @param Contract $identity
+     */
     public function __construct(
         private Contract $identity
     ){}
 
+    /**
+     * Summary of __invoke
+     * @param Request $request
+     * @return JsonResponse
+     */
     public function __invoke(Request $request): Response
     {
         $user = $this->identity->user(); 
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,22 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property ?Carbon $email_verified_at
+ * @property string|null $remember_token
+ */
 class User extends Authenticatable implements MustVerifyEmail, JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
